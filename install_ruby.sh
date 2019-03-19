035a5f34bc1a1910ec39453aae40394e00214e56
Edouard <etancelin.edouard@gmail.com>
2019-03-19 10:36:34 +0100

    Add log

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,9 +1,16 @@
-sudo rm -rf /usr/bin/ruby 
+echo "Delete local install"
+sudo rm -rf /usr/bin/ruby
+echo " Delete rbenv install" 
 sudo rm -rf $HOME/.rbenv /usr/local/rbenv /opt/rbenv /usr/local/opt/rbenv
+echo "Delete rbuild install" 
 brew uninstall --force rbenv ruby-build
+echo "Delete RBENV_VAR" 
 unset RBENV_ROOT && exec zsh
+echo "Install RBENV" 
 brew install rbenv ruby-build && exec zsh
+echo "Install ruby 2.3.7" 
 rbenv install 2.3.7
+echo "Install global 2.3.7" 
 rbenv global 2.3.7
 
 ruby -v
